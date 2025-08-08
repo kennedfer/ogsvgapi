64fa8445bc975a806d614dcd30083a7d3ea1b076
kennedfer <kenned14santos@gmail.com>
2025-08-07 22:09:31 -0300

    feat: Adiciona uma simples tela auxilar para a criação dos markdowns e svg's

---
+++ src/main/java/com/kennedfer/ogsvgapi/controllers/ViewController.java
@@ -0,0 +1,10 @@
+package com.kennedfer.ogsvgapi.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ViewController {
+    public String index(){
+        return "index";
+    }
+}
